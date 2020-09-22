ff5a0724d34452756a20c5ec38d397a2710085f7
ArtistVan1 <ytjunjie2006@126.com>
2020-09-22 13:22:37 +0900

    2020/9/22

--- day1/re1.py
+++ day1/re1.py
@@ -7,9 +7,11 @@ import numpy as np
 x_data = datasets.load_iris().data
 y_data = datasets.load_iris().target
 
+print(y_data)
+
 np.random.seed(116)
 np.random.shuffle(x_data)
-np.random.seed(116)
+np.random.seed(y_data)
 np.random.shuffle(y_data)
 tf.random.set_seed(116)
 
@@ -18,46 +20,47 @@ y_train = y_data[0:-30]
 x_test = x_data[-30:]
 y_test = y_data[-30:]
 
-x_train = tf.cast(x_train,dtype=tf.float32)
-x_test = tf.cast(x_test,dtype = tf.float32)
+x_train = tf.cast(x_train,tf.float32)
+x_test = tf.cast(x_test,tf.float32)
+
+w1 = tf.Variable(tf.random.truncated_normal([4,3],mean=0.1,stddev=0.1))
+b1 = tf.Variable(tf.random.truncated_normal([3],mean=0.1,stddev=0.1))
 
 train_db = tf.data.Dataset.from_tensor_slices((x_train,y_train)).batch(32)
 test_db = tf.data.Dataset.from_tensor_slices((x_test,y_test)).batch(32)
 
-w1 = tf.Variable(tf.random.truncated_normal([4,3],stddev=0.1,seed=1))
-b1 = tf.Variable(tf.random.truncated_normal([3],stddev=0.1,seed=1))
-
+epochs = 500
 lr = 0.1
-train_loss_results = []
+train_loss = []
+loss_all=0
 test_acc = []
-epoch = 500
-loss_all =0
-
-for epoch in range(epoch):
-    for step,(x_train,y_train) in enumerate(train_db):
+for step in range(epochs):
+    for index,(train,label) in enumerate(train_db):
         with tf.GradientTape() as tape:
-            y = tf.matmul(x_train,w1) + b1
-            y = tf.nn.softmax(y)
-            y_ = tf.one_hot(y_train,depth=3)
-            loss = tf.reduce_mean(tf.square(y_-y))
-            loss_all += loss.numpy()
+            y_ = tf.matmul(train,w1) + b1
+            y_ = tf.nn.softmax(y_)
+            label = tf.one_hot(label,depth=3)
+            loss = tf.reduce_mean(tf.square(y_-label))
+            loss_all +=loss.numpy()
+        grad = tape.gradient(loss,[w1,b1])
 
-        grads = tape.gradient(loss,[w1,b1])
-
-        w1.assign_sub(lr*grads[0])
-        b1.assign_sub(lr*grads[1])
-
-    print("Epoch {}, loss: {}".format(epoch, loss_all/4))
-    train_loss_results.append(loss_all / 4)
+        w1.assign_sub(grad[0]*lr)
+        b1.assign_sub(grad[1]*lr)
+    print("Epoch:{}, Loss:{}".format(step,loss_all/4))
+    train_loss.append(loss_all/4)
     loss_all = 0
 
-    total_correct, total_number = 0,0
-    for x_test,y_test in test_db:
-        y = tf.matmul(x_test,w1)+b1
+    # 测试部分
+    # total_correct为预测对的样本个数, total_number为测试的总样本数，将这两个变量都初始化为0
+    total_correct, total_number = 0, 0
+    for x_test, y_test in test_db:
+        # 使用更新后的参数进行预测
+        y = tf.matmul(x_test, w1) + b1
         y = tf.nn.softmax(y)
-        pred = tf.argmax(y,axis=1)
-
-        pred = tf.cast(pred,dtype=y_test.dtype)
+        pred = tf.argmax(y, axis=1)  # 返回y中最大值的索引，即预测的分类
+        # 将pred转换为y_test的数据类型
+        pred = tf.cast(pred, dtype=y_test.dtype)
+        # 若分类正确，则correct=1，否则为0，将bool型的结果转换为int型
         correct = tf.cast(tf.equal(pred, y_test), dtype=tf.int32)
         # 将每个batch的correct数加起来
         correct = tf.reduce_sum(correct)
@@ -75,7 +78,7 @@ for epoch in range(epoch):
 plt.title('Loss Function Curve')  # 图片标题
 plt.xlabel('Epoch')  # x轴变量名称
 plt.ylabel('Loss')  # y轴变量名称
-plt.plot(train_loss_results, label="$Loss$")  # 逐点画出trian_loss_results值并连线，连线图标是Loss
+plt.plot(train_loss, label="$Loss$")  # 逐点画出trian_loss_results值并连线，连线图标是Loss
 plt.legend()  # 画出曲线图标
 plt.show()  # 画出图像
 
@@ -86,29 +89,6 @@ plt.ylabel('Acc')  # y轴变量名称
 plt.plot(test_acc, label="$Accuracy$")  # 逐点画出test_acc值并连线，连线图标是Accuracy
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
